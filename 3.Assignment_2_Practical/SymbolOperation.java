19a5f5ff24cfe72be81281832dabd519aa84aeb0
Ajay-Git-Code <ajaysahani389852@gmail.com>
2021-04-06 12:59:55 +0530

    question 1 Symbol Operation

--- 3.Assignment_2_Practical/SymbolOperation.java
+++ 3.Assignment_2_Practical/SymbolOperation.java
@@ -1,4 +1,53 @@
-class SymbolOperation
+import java.util.Scanner;
+
+class Symbol
 {
+	private int a,b;
+	private String str  = new String();
+		
+	public void input()
+	{
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter two number and Symbol");
+		a = s.nextInt();
+		b = s.nextInt();
+		str = s.next();
+	}
 	
+	public void operation()
+	{
+		switch(str)
+		{
+			case "+":
+				System.out.println(a+b);
+				break;
+			case "-":
+				System.out.println(a-b);
+				break;
+			case "x":
+				System.out.println(a*b);
+				break;
+			case "/":
+				System.out.println(b/a);
+				break;
+			default:
+				System.out.println("Invalid operator");
+		}
+	}
+}
+
+public class SymbolOperation
+{
+	public static void main(String []args)
+	{
+		Scanner s = new Scanner(System.in);
+		boolean b;
+		Symbol a = new Symbol();
+		do
+		{
+			a.input();
+			a.operation();
+			b = s.nextBoolean();
+		}while(b !=false);
+	}
 }
